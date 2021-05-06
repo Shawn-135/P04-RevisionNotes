343f1911832a7228c297a0c25ca964e6419fbb6e
19031965 <19031965@myrp.edu.sg>
2021-05-06 15:18:52 +0800

    commit

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ This is the skeleton project for C347 P04
 1. Fork this project to your own GitHub account
 2. Check out the projecy from your own GitHub account
 3. Proceed to complete and check in/push your code into your GitHub Account
-
+ i love big penis
--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -1,15 +1,31 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class SecondActivity extends AppCompatActivity {
+
+	ListView lv;
+	ArrayAdapter aa;
+	ArrayList<Note> al;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//TODO implement the Custom ListView
+		lv = findViewById(R.id.lv);
 
+		Intent i = getIntent();
+
+		al = new ArrayList<Note>();
+		al.add(new Note());
 	}
 
 
